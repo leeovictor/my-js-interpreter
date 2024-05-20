bc92bc7c758af89bd7e15a2916d491078ad43db3
Leonardo Victor <leonardovictormarques@gmail.com>
2024-05-20 20:33:04 -0300

    add parser again :(

---
+++ src/parser.js
@@ -0,0 +1,171 @@
+import { TokenType } from "./token-type.js";
+import { Binary, Grouping, Literal, Unary } from "./expr.js";
+import { errorParser } from "./index.js";
+
+class ParserError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ParserError";
+  }
+}
+
+export class Parser {
+  /**
+   * @param {Token[]} tokens
+   */
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.current = 0;
+  }
+
+  parse() {
+    try {
+      return this.expression();
+      // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      return null;
+    }
+  }
+
+  expression() {
+    return this.equality();
+  }
+
+  equality() {
+    let expr = this.comparison();
+    if (this.match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+      const operator = this.previous();
+      const right = this.comparison();
+      expr = new Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  comparison() {
+    let expr = this.term();
+    while (
+      this.match(
+        TokenType.GREATER,
+        TokenType.GREATER_EQUAL,
+        TokenType.LESS,
+        TokenType.LESS_EQUAL,
+      )
+    ) {
+      const operator = this.previous();
+      const right = this.term();
+      expr = new Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  term() {
+    let expr = this.factor();
+    while (this.match(TokenType.MINUS, TokenType.PLUS)) {
+      const operator = this.previous();
+      const right = this.factor();
+      expr = new Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  factor() {
+    let expr = this.unary();
+    while (this.match(TokenType.SLASH, TokenType.STAR)) {
+      const operator = this.previous();
+      const right = this.unary();
+      expr = new Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  unary() {
+    if (this.match(TokenType.BANG, TokenType.MINUS)) {
+      const operator = this.previous();
+      const right = this.unary();
+      return new Unary(operator, right);
+    }
+    return this.primary();
+  }
+
+  primary() {
+    if (this.match(TokenType.FALSE)) return new Literal(false);
+    if (this.match(TokenType.TRUE)) return new Literal(true);
+    if (this.match(TokenType.NIL)) return new Literal(null);
+
+    if (this.match(TokenType.NUMBER, TokenType.STRING)) {
+      return new Literal(this.previous().literal);
+    }
+    if (this.match(TokenType.LEFT_PAREN)) {
+      const expr = this.expression();
+      this.consume(TokenType.RIGHT_PAREN, "Expect ')' after expression");
+      return new Grouping(expr);
+    }
+
+    throw this.error(this.peek(), "Expect expression");
+  }
+
+  consume(tokenType, message) {
+    if (this.check(tokenType)) return this.advance();
+
+    throw this.error(this.peek(), message);
+  }
+
+  error(token, message) {
+    errorParser(token, message);
+    return new ParserError(message);
+  }
+
+  synchronize() {
+    this.advance();
+    while (!this.isAtEnd()) {
+      if (this.previous().type === TokenType.SEMICOLON) return;
+
+      switch (this.peek().type) {
+        case TokenType.CLASS:
+        case TokenType.FUN:
+        case TokenType.VAR:
+        case TokenType.FOR:
+        case TokenType.IF:
+        case TokenType.WHILE:
+        case TokenType.PRINT:
+        case TokenType.RETURN:
+          return;
+      }
+      this.advance();
+    }
+  }
+
+  match(...tokenTypes) {
+    for (const type of tokenTypes) {
+      if (this.check(type)) {
+        this.advance();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  check(type) {
+    if (this.isAtEnd()) return false;
+    return this.peek().type === type;
+  }
+
+  advance() {
+    if (!this.isAtEnd()) {
+      this.current++;
+    }
+    return this.previous();
+  }
+
+  isAtEnd() {
+    return this.peek().type === TokenType.EOF;
+  }
+
+  peek() {
+    return this.tokens[this.current];
+  }
+
+  previous() {
+    return this.tokens[this.current - 1];
+  }
+}
