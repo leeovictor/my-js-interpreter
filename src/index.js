bdafbd4d12f55980b4455beb6ff0bb4ee0c03a73
Leonardo Victor <leonardovictormarques@gmail.com>
2024-05-20 16:24:44 -0300

    add scanner

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("hello");
+import { readFile } from "fs/promises";
+import { Scanner } from "./scanner.js";
+
+let hadError = false;
+
+const run = (source) => {
+  const scanner = new Scanner(source);
+  const tokens = scanner.scanTokens();
+
+  for (const token of tokens) {
+    console.log(token.toString());
+  }
+};
+
+export const error = (line, message) => {
+  report(line, "", message);
+};
+
+const report = (line, where, message) => {
+  console.log(`[line ${line}] Error${where}: ${message}`);
+  hadError = true;
+};
+
+if (!process.argv[2]) {
+  console.log("Error: source file  not provided");
+  process.exit(1);
+}
+
+try {
+  const source = await readFile(process.argv[2], { encoding: "utf-8" });
+  run(source);
+  if (hadError) {
+    process.exit(1);
+  }
+} catch (err) {
+  console.log(err);
+  process.exit(1);
+}
---
+++ src/scanner.js
@@ -0,0 +1,221 @@
+import { Token } from "./token.js";
+import { TokenType } from "./token-type.js";
+import * as Lev from "./index.js";
+
+export class Scanner {
+  /**
+   * @param {string} source
+   */
+  constructor(source) {
+    this.source = source;
+    this.tokens = [];
+
+    this.start = 0;
+    this.current = 0;
+    this.line = 1;
+
+    this.reservedWords = {
+      and: TokenType.AND,
+      class: TokenType.CLASS,
+      else: TokenType.ELSE,
+      false: TokenType.FALSE,
+      for: TokenType.FOR,
+      fun: TokenType.FUN,
+      if: TokenType.IF,
+      nil: TokenType.NIL,
+      or: TokenType.OR,
+      print: TokenType.PRINT,
+      return: TokenType.RETURN,
+      super: TokenType.SUPER,
+      this: TokenType.THIS,
+      true: TokenType.TRUE,
+      var: TokenType.VAR,
+      while: TokenType.WHILE,
+    };
+  }
+
+  scanTokens() {
+    while (!this.isAtEnd()) {
+      this.start = this.current;
+      this.scanToken();
+    }
+    this.addToken(TokenType.EOF);
+    return this.tokens;
+  }
+
+  scanToken() {
+    const c = this.advance();
+    switch (c) {
+      case "(":
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case ")":
+        this.addToken(TokenType.RIGHT_PAREN);
+        break;
+      case "{":
+        this.addToken(TokenType.LEFT_BRACE);
+        break;
+      case "}":
+        this.addToken(TokenType.RIGHT_BRACE);
+        break;
+      case ",":
+        this.addToken(TokenType.COMMA);
+        break;
+      case ".":
+        this.addToken(TokenType.DOT);
+        break;
+      case "-":
+        this.addToken(TokenType.MINUS);
+        break;
+      case "+":
+        this.addToken(TokenType.PLUS);
+        break;
+      case ";":
+        this.addToken(TokenType.SEMICOLON);
+        break;
+      case "*":
+        this.addToken(TokenType.STAR);
+        break;
+      case "!":
+        this.addToken(this.match("=") ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case "=":
+        this.addToken(
+          this.match("=") ? TokenType.EQUAL_EQUAL : TokenType.EQUAL,
+        );
+        break;
+      case "<":
+        this.addToken(this.match("=") ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case ">":
+        this.addToken(
+          this.match("=") ? TokenType.GREATER_EQUAL : TokenType.GREATER,
+        );
+        break;
+      case "/":
+        if (this.match("/")) {
+          while (this.peek() !== "\n" && !this.isAtEnd()) {
+            this.advance();
+          }
+        } else {
+          this.addToken(TokenType.SLASH);
+        }
+        break;
+      case " ":
+      case "\t":
+      case "\r":
+        break;
+      case "\n":
+        this.line++;
+        break;
+      case '"':
+        this.string();
+        break;
+      default:
+        if (this.isDigit(c)) {
+          this.number();
+        } else if (this.isAlpha(c)) {
+          this.identifier();
+        } else {
+          Lev.error(this.line, "Unexpected character.");
+        }
+        break;
+    }
+  }
+
+  identifier() {
+    while (this.isAlphaNumeric(this.peek())) {
+      this.advance();
+    }
+
+    const text = this.source.substring(this.start, this.current);
+    this.addToken(this.reservedWords[text] ?? TokenType.IDENTIFIER);
+  }
+
+  isAlpha(c) {
+    return (c >= "a" && c <= "z") || (c >= "A" && c <= "Z") || c === "_";
+  }
+
+  isAlphaNumeric(c) {
+    return this.isAlpha(c) || this.isDigit(c);
+  }
+
+  number() {
+    while (this.isDigit(this.peek())) {
+      this.advance();
+    }
+
+    if (this.peek() === "." && this.isDigit(this.peekNext())) {
+      this.advance();
+      while (this.isDigit(this.peek())) {
+        this.advance();
+      }
+    }
+
+    this.addToken(
+      TokenType.NUMBER,
+      parseFloat(this.source.substring(this.start, this.current)),
+    );
+  }
+
+  peekNext() {
+    if (this.current + 1 >= this.source.length) return "\0";
+    return this.source.charAt(this.current + 1);
+  }
+
+  isDigit(c) {
+    return c >= "0" && c <= "9";
+  }
+
+  string() {
+    while (this.peek() !== '"' && !this.isAtEnd()) {
+      if (this.peek() === "\n") {
+        this.line++;
+      }
+      this.advance();
+    }
+
+    if (this.isAtEnd()) {
+      Lev.error(this.line, "Unterminated string.");
+      return;
+    }
+
+    this.advance();
+    const value = this.source.substring(this.start + 1, this.current - 1);
+    this.addToken(TokenType.STRING, value);
+  }
+
+  peek() {
+    if (this.isAtEnd()) return "\0";
+    return this.source.charAt(this.current);
+  }
+
+  match(expected) {
+    if (this.isAtEnd()) return false;
+    if (this.source.charAt(this.current) !== expected) return false;
+
+    this.current++;
+    return true;
+  }
+
+  isAtEnd() {
+    return this.current >= this.source.length;
+  }
+
+  advance() {
+    return this.source.charAt(this.current++);
+  }
+
+  addToken(type, literal) {
+    this.tokens.push(
+      new Token(
+        type,
+        type === TokenType.EOF
+          ? ""
+          : this.source.substring(this.start, this.current),
+        literal,
+        this.line,
+      ),
+    );
+  }
+}
